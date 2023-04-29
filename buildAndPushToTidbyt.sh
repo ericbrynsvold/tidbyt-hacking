b99c9ef0fd233fea1864c7265c7f467748d235aa
Eric Brynsvold <eric.brynsvold@gmail.com>
2023-04-28 22:23:56 -0500

    Add deployment info for kyle

--- buildAndPushToTidbyt.sh
+++ buildAndPushToTidbyt.sh
@@ -14,8 +14,12 @@ sourceFile="$directory/$appName.star"
 outputFile="$directory/$appName.webp"
 tidbytApiToken=$TIDBYT_API_TOKEN
 tidbytDeviceId=$TIDBYT_DEVICE_ID
+tidbytApiTokenKyle=$TIDBYT_API_TOKEN_KYLE
+tidbytDeviceIdKyle=$TIDBYT_DEVICE_ID_KYLE
 
 # build
 pixlet render $sourceFile
 # upload
 pixlet push --installation-id $appName $tidbytDeviceId $outputFile --api-token $tidbytApiToken
+# upload to Kyle
+pixlet push --installation-id $appName $tidbytDeviceIdKyle $outputFile --api-token $tidbytApiTokenKyle
